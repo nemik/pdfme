10e5423ed4f02668d2c185980f75331db4bd3bd3
Nemanja Stefanović <nemik@nemik.net>
2023-03-09 11:31:15 -0600

    fix this, otherwise zoom will be bad when editing

--- packages/ui/src/constants.ts
+++ packages/ui/src/constants.ts
@@ -2,7 +2,7 @@ export const DEFAULT_LANG = 'en' as const;
 
 export const DESTROYED_ERR_MSG = 'this instance is already destroyed';
 
-export const ZOOM = 5.7795275591;
+export const ZOOM = 3.7795275591;
 
 export const SELECTABLE_CLASSNAME = 'selectable';
 
